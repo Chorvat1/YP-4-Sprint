c07a0ef65342e671c73f8023faf6d6d3d5288ca1
Chorvat1 <106180768+Chorvat1@users.noreply.github.com>
2024-10-05 17:53:36 +0300

    Add files via upload

---
+++ Status.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.Финальные_спринты.Четвертый_Спринт;
+
+// Перечисление статусов задачи
+public enum Status {
+    NEW, // Новая
+    IN_PROGRESS, // В процессе
+    DONE // Завершена
+}
